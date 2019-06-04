cb0ee2e8038aa6c842f02d8e1398e2b5218f030f
Derya O. Kurin <dozdemir663@gmail.com>
2019-06-04 18:34:31 +0300

    fixing bug1

--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -1,9 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Wiki = sequelize.define('Wiki', {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING,
-    private: DataTypes.BOOLEAN
+    title: {
+      type: DataTypes.STRING,
+      allownull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allownull: false
+    },
+    private: DataTypes.BOOLEAN,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
   Wiki.associate = function(models) {
     // associations can be defined here
